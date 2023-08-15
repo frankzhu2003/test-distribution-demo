32217f79e9cfc5081a9a986be5ad2c2101951aef
Kyle Moore <kmoore@gradle.com>
2023-08-15 11:38:24 -0400

    Use Java toolchain feature

--- quick-demo/build.gradle.kts
+++ quick-demo/build.gradle.kts
@@ -13,8 +13,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 tasks.test {
